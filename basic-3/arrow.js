b84113f52274a81c45816706c0fe3dc8f68bdbff
ShaluPruthi <132157855+ShaluPruthi@users.noreply.github.com>
2025-02-24 06:03:16 +0000

    this, arrow

--- basic-3/arrow.js
+++ basic-3/arrow.js
@@ -19,3 +19,37 @@ user.name="Sam"
 user.welcomeMsg()   //changed value
 
 console.log("outside",this);    //doesn't contain any value return an empty object
+
+
+//function one(){
+//    let username= "Shalu"
+//    console.log(this.username); //undefined, we can not use this keyword inside the function
+//}
+//one()
+
+
+//const use = function(){
+//    let username = "shalu"
+//    console.log(this.username);   //undefined
+//}
+//use()
+
+
+//++++++++++++ ARROW FUNCTION +++++++++++++
+//syntax : () =>{}
+
+const addTwo = (num1, num2) => {
+    return num1 + num2
+}
+console.log("+++ ARROW FUNCTION START +++");
+console.log(addTwo(4,5))
+
+
+//also done with implicit return : when we use { } we need to write return but if use ( ) there is not need to write return
+const add = (num1, num2) => num1 + num2
+console.log(add(2,5))
+
+
+//return object
+const addThree = () => ({username : "shal"})
+console.log(addThree());
