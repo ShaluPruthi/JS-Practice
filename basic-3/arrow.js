50055fae9a5059217a1e4f36a191e12cc35be3ca
ShaluPruthi <132157855+ShaluPruthi@users.noreply.github.com>
2025-02-24 04:32:10 +0000

    more

--- basic-3/arrow.js
+++ basic-3/arrow.js
@@ -1,12 +1,14 @@
 //ARROW function ES6 was introduced in 2015 along with newer features in which we got arrow function
 //THROW keyword refer the current context of particular scope
+//Global object in browser is window object
 
 const user={
     name:"shalu",
     price: 999,
     
     welcomeMsg: function(){
-        console.log(`${this.name} , welcome to the site`); 
+        console.log(`${this.name} , welcome to the site`)
+        console.log("inside ",this)
         
     }
 
@@ -15,3 +17,5 @@ const user={
 user.welcomeMsg()   //default value
 user.name="Sam"
 user.welcomeMsg()   //changed value
+
+console.log("outside",this);    //doesn't contain any value return an empty object
