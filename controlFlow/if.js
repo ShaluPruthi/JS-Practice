d516c792d613669efada461fb2dcd608e8a21d01
ShaluPruthi <132157855+ShaluPruthi@users.noreply.github.com>
2025-02-27 05:37:06 +0000

    flow

---
+++ controlFlow/if.js
@@ -0,0 +1,29 @@
+//syntax
+// if(true_condition){
+//     //scope
+// }
+
+//comparison operator ~ <, >, <=, >=, ==(check if equal or not), !=, (= to assign), === data type and value
+
+if(2 == "2" ){
+    console.log("executed")
+}
+
+if(2 === "2"){
+    console.log("2 executed")
+}
+
+//if-else
+const temp = 41
+if(temp < 50){
+    console.log("less than 50");
+    
+}else{
+console.log("temp greater");
+}
+
+const score= 200
+if(score > 100){
+    const power = "fly";
+    console.log(`User Power: ${power}`);
+}
