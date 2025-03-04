ff8037fd9d2f9fe1c300a86a3612dfb582ffbe10
ShaluPruthi <132157855+ShaluPruthi@users.noreply.github.com>
2025-03-04 07:59:42 +0000

    for

---
+++ controlFlow/for.js
@@ -0,0 +1,9 @@
+// iterations syntax
+// for(let i= 0(initialize); i < array.length(condition); i++ (updation)){
+
+// }
+
+for (let index=0; index < 10; index++){
+    const element = index;
+    console.log(element);
+}
