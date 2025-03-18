d43cc7e74a1d37d53a0855be96602b1570a47b7a
ShaluPruthi <132157855+ShaluPruthi@users.noreply.github.com>
2025-03-18 18:06:17 +0000

    little

--- Projects/GuessNumber/script.js
+++ Projects/GuessNumber/script.js
@@ -13,3 +13,57 @@ let prevGuess = []
 let numGuesses = 1;
 
 let playGame = true;
+
+if(playGame){
+    submit.addEventListener('click', function(e){
+        e.preventDefault()
+        const guess = parseInt(input.value)
+        console.log(guess)
+        validateGuess(guess)
+    })
+}
+
+function validateGuess(guess){
+    //check validations 1-100
+    if(isNaN(guess)){
+        alert('please enter valid number')
+    }
+    else if(guess < 1){
+        alert('please enter number more than 1')
+    }
+    else if(guess > 100){
+        alert('please enter a number less than 100')
+    }
+    else{
+        prevGuess.push(guess)
+        if(numGuesses == 11){
+            displayGuess(guess)
+            displayMsg(`Game Over , Random number was ${rndmNo}`)
+            endGame()
+        }
+        else{
+            displayGuess(guess)
+            checkGuess(guess)
+        }
+    }
+}
+
+function checkGuess(guess){
+    //random no. not equal
+}
+
+function displayGuess(guess){
+    //clean values update remaining 
+}
+
+function displayMsg(msg){
+    //DOM interaction
+}
+
+function newGame(){
+
+}
+
+function endGame(){
+
+}
